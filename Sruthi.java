6d0512290aa2b20223980deed2e1335a76bcf7fe
naveenganji <naveen91.ganji@gmail.com>
2018-09-03 14:41:40 -0700

    Update Sruthi.java

--- Sruthi.java
+++ Sruthi.java
@@ -2,7 +2,11 @@ class Sruthi
 {
 public static void main (String args[])
 
+  for(int n=1;n<=10;n++)
 {
-System.out.println("hi naveen hru ate lunch hey");
+System.out.println("hi naveen hru ate lunch hey"+n);
+System.out.println("whatsup");
+  }
+}
 }
 }
