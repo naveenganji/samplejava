9780a6448864c4d27fbf2050bb482075517cc41d
naveenganji <naveen91.ganji@gmail.com>
2018-09-03 14:42:57 -0700

    Update Sruthi.java

--- Sruthi.java
+++ Sruthi.java
@@ -1,7 +1,7 @@
 class Sruthi
 {
 public static void main (String args[])
-
+{
   for(int n=1;n<=10;n++)
 {
 System.out.println("hi naveen hru ate lunch hey"+n);
@@ -9,4 +9,4 @@ System.out.println("whatsup");
   }
 }
 }
-}
+
